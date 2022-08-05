7a2fd6b4f30614047841d1c0ee2a03c4979004c4
Ashley <asksmashley@gmail.com>
2022-08-05 01:11:52 -0700

    push changes

--- pages/components/SidePanel.js
+++ pages/components/SidePanel.js
@@ -6,6 +6,7 @@ const SidePanel = () => {
     // fix top of the scrollbar
     // add function to bookmark/save results and display bookmarked items somewhere on page 
       // model with buttom?? 
+      // initialize floating button that updates with number of saved places 
     <div className="fixed inset-y-0 hidden w-[42rem] pt-20 mb-4 overflow-x-hidden overflow-y-auto scrollbar sm:block">
       <div className="min-h-full pt-8 border-r-2 shadow-md border-r-gray-200">
         <div className="flex flex-col gap-2 mx-4">
